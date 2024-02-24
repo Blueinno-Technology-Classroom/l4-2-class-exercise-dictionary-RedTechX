a2763f4a63bf5c391fb7d0657c4d6abc8e983f54
RedTechX <jeremy.fangyc@gmail.com>
2024-02-24 15:08:15 +0800

    TghkjU^TRtvgU^tyDVGIYt6R&tugGIY^%7tU$t&6898iyuhgGfiR*^7*&I&UYTRE5yUE%&^7iu

--- main.py
+++ main.py
@@ -2,9 +2,17 @@
 '''
 Q1: 
 '''
+hermione_portfolio = {
+   "name": "Hermione",
+   "marks": {
+       "transfiguration": 90,
+       "charms": 98,
+       "potions": 98,
+       "history of magic": 92,
+   }
+}
 
-# TODO: Write your code here
-
+print(hermione_portfolio["marks"]['history of magic'])
 ##################################################
 '''
 Q2:
@@ -12,6 +20,13 @@ Q2:
 
 # TODO: Write your code here
 
+moneymoney = {
+    'emp1': {'name': 'Jhon', 'salary': 7500},
+    'emp2': {'name': 'Emma', 'salary': 8000},
+    'emp3': {'name': 'Brad', 'salary': 500}
+}
+
+moneymoney['emp3']['salary'] = 8500
 ##################################################
 '''
 Q3:
